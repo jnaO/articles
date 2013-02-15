71e9fad1455842f33451dad55bea4879736d0e9c
Jan Lofberg <chenebu@gmail.com>
2013-02-15 22:36:32 +0100

    fixed shadow in range

---
+++ WebGL/webgl.php
@@ -0,0 +1,76 @@
+    <!-- the id is used as title of post -->
+    <article id="SVG" data-mtest="inlinesvg" class="post">
+      <label class="post-title">SVG</label>
+
+      <svg class="svg-canvas" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="260px" height="140px" viewBox="0 0 260 140"
+   overflow="hidden">
+        <defs>
+          <linearGradient id="audio-led" gradientUnits="userSpaceOnUse" x1="0" y1="2" x2="0" y2="120">
+            <stop  offset="0.1" style="stop-color:#ff6700"/>
+            <stop  offset="0.2" style="stop-color:#f9c900"/>
+            <stop  offset="1" style="stop-color:#80802a"/>
+          </linearGradient>
+        </defs>
+        <rect y="40" width="19" height="160"  fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="20" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="40" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="60" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="80" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="100" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="120" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="140" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="160" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="180" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="200" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="220" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+        <rect y="40" x="240" width="19" height="160" fill="url(#audio-led)">
+          <animate attributeName="y" to="0" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseover" calcMode="spline" fill="freeze"/>
+          <animate attributeName="y" to="40" dur="0.7s" keySplines="0.1 0.05 0.5 1" begin="mouseout" calcMode="spline" fill="freeze"/>
+        </rect>
+
+      </svg>
+      <dl class="post-description">
+        <dt>i:</dt>
+        <dd><a onclick="_gaq.push(['_trackEvent', 'Social', 'sharp sliders', 'twitter'])" class="twitter">@chenebu</a></dd>
+        <dt>inspiration:</dt>
+        <dd><a onclick="_gaq.push(['_trackEvent', 'External', 'sharp sliders', 'inspiration'])" href="http://dribbble.com/shots/317275-Sharp-Sliders">sharp sliders</a></dd>
+        <dt>in my earphones:</dt>
+        <dd><a  onclick="_gaq.push(['_trackEvent', 'Music', 'sharp sliders', 'kite 4'])" href="http://open.spotify.com/album/1GrfYZSn0DVd4aGZ8YIkCC">kite 4 (spotify link)</a></dd>
+      </dl>
+    </article>
---
+++ clippaths/clp.php
@@ -0,0 +1,53 @@
+    <!-- the id is used as title of post -->
+    <article id="Type0" data-mtest="inputtypes.range" class="post clip-box">
+      <label class="post-title">Type0</label>
+
+      <div class="wrap">
+        <div class="svg-wrap">
+          <p>Make</p>
+          <p>Enjoy</p>
+          <p>Discover</p>
+
+          <svg class="svg-graphic" width="453" height="513" viewBox="0 0 2824 2052" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+            <g>
+              <clipPath id="hex-mask">
+                <path d="M802.205,87.874L0,2032.454h447.874l354.331-881.587l345.827,881.587h456.378l-372.861-914.787
+  c0,0,20.732-25.209,18.667-72.001c21.641-31.541,112.922-44.873,75.334-118c-36.668-71.333-96-24.666-98.667-22
+  c-12.667-10-18.001-62.666-52-88c-34-25.333-36.667-22-36.667-22s0.667-11.999-9.333-16.666c2-53.334,48.589-779,48.589-779
+  l-133.229,649.134L802.205,87.874z" />
+              </clipPath>
+              <clipPath id="inner-mask">
+                <polygon points="459.212,2032.454 680.286,2032.454 800.771,1735.525 915.786,2032.454 1136.693,2032.454 802.205,1188.525 "/>
+              </clipPath>
+            </g>
+
+            <a xlink:href="http://shakenandstirredweb.com">
+              <path fill="#ff9c21" d="M802.205,87.874L0,2032.454h447.874l354.331-881.587l345.827,881.587h456.378l-372.861-914.787
+  c0,0,20.732-25.209,18.667-72.001c21.641-31.541,112.922-44.873,75.334-118c-36.668-71.333-96-24.666-98.667-22
+  c-12.667-10-18.001-62.666-52-88c-34-25.333-36.667-22-36.667-22s0.667-11.999-9.333-16.666c2-53.334,48.589-779,48.589-779
+  l-133.229,649.134L802.205,87.874z" transform="translate(1, 1)" />
+              <image clip-path="url(#hex-mask)" height="100%" width="100%" xlink:href="articles/clippaths/img/outer.png" preserveAspectRatio="xMidYMin slice" />
+            </a>
+
+            <a xlink:href="http://sawyerh.com" transform="translate(0, 0)">
+              <polygon fill="#fff" points="459.212,2032.454 680.286,2032.454 800.771,1735.525 915.786,2032.454 1136.693,2032.454 802.205,1188.525" transform="translate(1, 1)" />
+              <image clip-path="url(#inner-mask)" height="100%" width="100%" xlink:href="articles/clippaths/img/inner.png" preserveAspectRatio="xMidYMin slice" />
+            </a>
+
+            <!-- <a xlink:href="http://www.organizedwonder.com" transform="scale(0.5) translate(270, 315)" >
+              <polygon fill="#64dcff" points="268,0 0,158 0,483 268,644 558,483 558,158" transform="translate(1, 1)" />
+              <image clip-path="url(#hex-mask)" height="100%" width="100%" xlink:href="articles/clippaths/img/fountain.jpeg" preserveAspectRatio="xMidYMin slice"/>
+            </a> -->
+          </svg>
+        </div><!-- #svg-wrap -->
+      </div>
+
+      <dl class="post-description">
+        <dt>i:</dt>
+        <dd><a onclick="_gaq.push(['_trackEvent', 'Social', 'ppl', 'twitter'])" class="twitter">@chenebu</a></dd>
+        <dt>in my earphones:</dt>
+        <dd><a  onclick="_gaq.push(['_trackEvent', 'Music', 'ppl', 'Mile of Solace'])" href="http://open.spotify.com/artist/7Kx0eIQ6tAPnZuaQT0PEJd">Mile of Solace (spotify link)</a></dd>
+        <dt>Requirements:</dt>
+        <dd><a href="https://www.google.com/chrome" title="Download Goole Chrome web browser">Google Chrome</a>, <a href="https://tools.google.com/dlpage/chromesxs" title="Download developer version of google chrome">Google Chrome Canary</a></dd>
+      </dl>
+    </article>
